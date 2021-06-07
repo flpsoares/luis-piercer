22dd08dd61b417cc6ecea7d667729d7f80da6064
Filipe Soares <filipeseventeen1@gmail.com>
2021-06-07 19:11:17 -0300

    Adicionado os estilos globais ao projeto

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button, input {
+    background: 0;
+    border: 0;
+  }
+
+  :root {
+    --primary: #E78D3C;
+    --dark: #1f2125;
+  }
+`
