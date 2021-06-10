732695bc8de2538b3bbdbaf8e08cd3dcc7c3d7e1
Vito Borba <vito.borba@gmail.com>
2021-06-10 05:01:32 -0300

    Sessão dos bignumbers com icones

--- src/components/Galery/style.ts
+++ src/components/Galery/style.ts
@@ -11,9 +11,13 @@ export const SquareGalery = styled.div`
   height: 400px;
 `
 
-export const WhyMe = styled.div`
+export const WhyMeContainer = styled.div`
   width: 100%;
   color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
   h2 {
     font-size: 50px;
     margin-bottom: 20px;
@@ -25,10 +29,54 @@ export const WhyMe = styled.div`
   }
 `
 
+export const WhyMe = styled.div`
+  width: 50%;
+`
+
+export const BigNumbers = styled.div`
+  width: 50%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    width: 100%;
+    display: flex;
+    padding: 6px;
+  }
+`
+
+export const BoxBigNumbers = styled.div`
+  width: 50%;
+  /* background-color: blue; */
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    h2 {
+      margin: 0;
+      font-size: 20px;
+      font-weight: 500;
+      color: var(--primary);
+    }
+    p {
+      margin: 5px;
+    }
+  }
+`
+
 export const CarouselImage = styled.div`
   img {
     width: 170px;
     height: 100%;
+    border: 0px solid white;
 
     margin: 0 40px;
   }
